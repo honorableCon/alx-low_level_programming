88e420029c9c214f4b1a5557a005db5c9525b96f
honorablecon <bayeyoung@gmail.com>
2022-09-13 14:08:05 -0700

    Write a function that checks for lowercase character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to test
+ *
+ * Return: 1 If c is lowercase.
+ * Otherwise, 0 is returned.
+ */
+int _islower(int c)
+{
+	return ((c >= (int)'a' && c <= (int)'a' + 26));
+}
