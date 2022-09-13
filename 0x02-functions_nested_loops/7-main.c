0ddc3ace3b1a1b796027b8b9c5cc17fc468995e0
honorablecon <bayeyoung@gmail.com>
2022-09-13 15:46:35 -0700

    fix limit

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <limits.h>
+#include <stdio.h>
 
 /**
  * main - check the code
@@ -8,10 +10,10 @@
 int main(void)
 {
     int r;
-
+	
     print_last_digit(98);
     print_last_digit(0);
-    r = print_last_digit(-1024);
+    r = print_last_digit(INT_MIN);
     _putchar('0' + r);
     _putchar('\n');
     return (0);
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,7 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit = ((n < 0) ? n * -1 : n) % 10;
+	int last_digit = ((n < 0) ? (n + 10) * -1 : n) % 10;
 
 	_putchar(last_digit + '0');
 	return (last_digit);
