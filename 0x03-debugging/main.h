2a3da2fbf02f9bbcce739d03c7a92738ab6aedd3
Honorable con <honorablecon@gmail.com>
2022-09-15 00:42:22 +0000

    fix function declaration

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,5 +5,7 @@
 
 int largest_number(int, int, int);
 void positive_or_negative(int);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
