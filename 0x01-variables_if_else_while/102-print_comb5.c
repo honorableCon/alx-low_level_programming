44cee759b72d25451cc6dc51530eb72e4c4a0895
honorablecon <bayeyoung@gmail.com>
2022-09-19 07:44:59 -0700

    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print combinations of two-digits
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	putchar('0' + 0);
+	putchar('0' + 0);
+	putchar(' ');
+	putchar('0' + 0);
+	putchar('0' + 1);
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 /**
- * reset_to_98 - Write a function that takes a pointer 
- * to an int as parameter and updates 
+ * reset_to_98 - Write a function that takes a pointer
+ * to an int as parameter and updates
  * the value it points to to 98.
  *
+ * @n: number to change
  */
 
 
